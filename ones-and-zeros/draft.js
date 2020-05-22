9c77b55f386823e308ee95f149ddbacb3174cb5a
Orion-P <orionpoptean@gmail.com>
2020-05-22 09:48:48 -0700

    Created a small program that converts an array of binary numbers to a base 10 number.

---
+++ ones-and-zeros/draft.js
@@ -0,0 +1,17 @@
+
+/* Draft
+const binaryArrayToNumber = arr => {
+  let output = 0;
+  let j = 0;
+  for (i = arr.length - 1; i >= 0; i--) {
+    let value = arr[i] * (2 ** j)
+    j++;
+    console.log(value)
+    output += value;
+}
+
+  return output;
+};
+
+*/
+
